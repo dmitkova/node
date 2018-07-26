f4066e2221235161c11fe7d340b25bcfe36eebb0
dmitkova <39170253+dmitkova@users.noreply.github.com>
2018-07-26 21:33:08 +0200

    1 cas node js

---
+++ index.js
@@ -0,0 +1,60 @@
+// console.log("hello node js");
+
+// var integer1 = 5;
+// var integer2 = 10;
+
+// console.log(integer1 + "+" + integer2);
+
+// var name = "Dragana";
+// var pass = "0000";
+// var nameUpdated = name.toLowerCase();
+
+// if(nameUpdated == "dragana"){
+// 	console.log("Hello " + name);
+// }
+// else{
+// 	console.log("unsucessfull")
+// }
+
+// switch(name){
+// 	case "Dragana":
+// 	console.log("Hello " + name);
+// 	break;
+// 	case "DRAGANA"
+// 	console.log("Hello " + name);
+// 	break;
+// 	default:
+// 	console.log("unsucessfull")
+// }
+
+
+// if(name == "Dragana" ){
+// 	console.log("hello " + name);
+// }
+// else{
+// 	console.log("unsucessfull");
+// }
+
+var firstName = "Dragana";
+var lastName = "Mitkova";
+var fullName = firstName.concat(lastName);
+console.log(fullName);
+var boolean = true;
+console.log(boolean);
+
+
+var integer = [3, 4, 5, 6, 7, 8, 9, 10, 12];
+
+integer[100] = 50;
+console.log(integer);
+
+// for(let i=0; i < integer.length; i++){
+// 	console.log(integer[i] +":");
+
+// 	if(integer[i] % 3 == 0){
+// 		console.log("buzz")
+// 	}
+// 	if(integer[i] % 4 == 0){
+// 		console.log("fizz")
+// 	}
+// }
