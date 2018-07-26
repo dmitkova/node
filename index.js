7a51f781b729bdc287447909c417fe1c7252339f
dmitkova <39170253+dmitkova@users.noreply.github.com>
2018-07-26 22:07:30 +0200

    cas1

--- index.js
+++ index.js
@@ -35,18 +35,18 @@
 // 	console.log("unsucessfull");
 // }
 
-var firstName = "Dragana";
-var lastName = "Mitkova";
-var fullName = firstName.concat(lastName);
-console.log(fullName);
-var boolean = true;
-console.log(boolean);
+// var firstName = "Dragana";
+// var lastName = "Mitkova";
+// var fullName = firstName.concat(lastName);
+// console.log(fullName);
+// var boolean = true;
+// console.log(boolean);
 
 
-var integer = [3, 4, 5, 6, 7, 8, 9, 10, 12];
+// var integer = [3, 4, 5, 6, 7, 8, 9, 10, 12];
 
-integer[100] = 50;
-console.log(integer);
+// integer[100] = 50;
+// console.log(integer);
 
 // for(let i=0; i < integer.length; i++){
 // 	console.log(integer[i] +":");
@@ -58,3 +58,33 @@ console.log(integer);
 // 		console.log("fizz")
 // 	}
 // }
+
+// var brojac = 0;
+// for(let i = 10; i > brojac; i--){
+// 	console.log(i);
+// }
+// while(brojac <10){
+// 	brojac++;
+// 	console.log(brojac);
+	
+// }
+
+// do {
+// 	brojac++;
+// 	console.log(brojac);
+// }
+// while(brojac < 10)
+
+var names =['iva', 'ana', 'marija'];
+var pass =['a', 'bb', 'ccc']
+var myName = 'ana';
+var myPass ='bbb'
+for(let i= 0; i<names.length; i++){
+
+	if(names[i] == myName && pass[i] == myPass){
+		console.log("logged in");
+	else {
+		console.log("error")
+	}
+	}
+}
